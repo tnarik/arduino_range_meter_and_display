81e89220e18ac4897340e7a8b2f639779eebcd28
tnarik <tnarik@gmail.com>
2014-04-25 08:48:29 +0100

    Inclusion of the SPI library for display purposes (requires library integration in the IDE).

--- arduino_range_meter_and_display.ino
+++ arduino_range_meter_and_display.ino
@@ -1,7 +1,10 @@
 // Do not remove the include below
 #include "arduino_range_meter_and_display.h"
 
+#include <SPI.h>
+
 const int sonar_pwm_pin    = 3;
+int csPin = 10; //You can use any IO pin but for this example we use 10
 
 int pwm_val () {
   unsigned long raw_pwm_value = pulseIn(sonar_pwm_pin, HIGH);
@@ -17,6 +20,13 @@ int pwm_val () {
 void setup() {
   Serial.begin(9600);
   pinMode(sonar_pwm_pin, INPUT);
+
+  SPI.begin(); //Start the SPI hardware
+  SPI.setClockDivider(SPI_CLOCK_DIV64); //Slow down the master a bit
+
+  //Send the reset command to the display - this forces the cursor to return to the beginning of the display
+  digitalWrite(csPin, LOW); //Drive the CS pin low to select OpenSegment
+  SPI.transfer('v'); //Reset command
 }
 
 // The loop function is called in an endless loop
