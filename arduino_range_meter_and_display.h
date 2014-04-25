187029e7d5ad957921c40d64bee48caed3159809
tnarik <tnarik@gmail.com>
2014-04-25 07:49:28 +0100

    Updated with actual code.

--- arduino_range_meter_and_display.h
+++ arduino_range_meter_and_display.h
@@ -9,7 +9,6 @@
 #include "Arduino.h"
 //add your includes for the project arduino_range_meter_and_display here
 
-
 //end of add your includes here
 #ifdef __cplusplus
 extern "C" {
@@ -21,6 +20,7 @@ void setup();
 #endif
 
 //add your function definitions for the project arduino_range_meter_and_display here
+int pwm_val ();
 
 
 
--- arduino_range_meter_and_display.ino
+++ arduino_range_meter_and_display.ino
@@ -1,15 +1,28 @@
 // Do not remove the include below
 #include "arduino_range_meter_and_display.h"
 
+const int sonar_pwm_pin    = 3;
+
+int pwm_val () {
+  unsigned long raw_pwm_value = pulseIn(sonar_pwm_pin, HIGH);
+  //Serial.print("pwm: ");
+  //Serial.println(raw_pwm_value);
+  //Serial.println( raw_pwm_value/57.87);
+  // Datasheet: PW - This pin outputs a pulse width representation of range.
+  // To calculate distance use the scale factor of 147mS per inch ( 57.87 per cm)
+  return raw_pwm_value/57.87;
+}
 
 //The setup function is called once at startup of the sketch
-void setup()
-{
-// Add your initialization code here
+void setup() {
+  Serial.begin(9600);
+  pinMode(sonar_pwm_pin, INPUT);
 }
 
 // The loop function is called in an endless loop
-void loop()
-{
-//Add your repeated code here
+void loop() {
+  int cms = pwm_val();
+  Serial.println(cms);
+  delay(100);
+
 }
