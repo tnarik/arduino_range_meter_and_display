fc93949268d3790d91d5cc73ec1aee9de3f95cf1
tnarik <tnarik@gmail.com>
2014-04-26 14:42:52 +0100

    Updated to latest Arduino (1.5.6r2) and latest Eclipse plugin (pull request #140). Added .fzz diagram with all parts.

--- arduino_range_meter_and_display.ino
+++ arduino_range_meter_and_display.ino
@@ -4,16 +4,47 @@
 #include <SPI.h>
 
 const int sonar_pwm_pin    = 3;
-int csPin = 10; //You can use any IO pin but for this example we use 10
+// the Arduino default constant 'SS' is used (10). Any other IO pin could be used
+//const int slave_select_pin = 10;
 
 int pwm_val () {
   unsigned long raw_pwm_value = pulseIn(sonar_pwm_pin, HIGH);
-  //Serial.print("pwm: ");
-  //Serial.println(raw_pwm_value);
-  //Serial.println( raw_pwm_value/57.87);
-  // Datasheet: PW - This pin outputs a pulse width representation of range.
-  // To calculate distance use the scale factor of 147mS per inch ( 57.87 per cm)
+  // From the datasheet: PW - This pin outputs a pulse width representation of range.
+  // To calculate distance use the scale factor of 147mS per inch (57.87 per cm)
   return raw_pwm_value/57.87;
+}
+
+void spi_out_command(byte command, byte data) {
+  digitalWrite(SS, LOW); // enlow to select
+  SPI.transfer(command);    // command
+  if ( command != 0x76 && command != 0x81 ) {
+    SPI.transfer(data);    // data
+  }
+  digitalWrite(SS, HIGH); // high to deselect
+}
+
+void spi_out_display(byte digit1, byte digit2, byte digit3, byte digit4) {
+  digitalWrite(SS, LOW); // low to select
+  SPI.transfer(digit1);
+  SPI.transfer(digit2);
+  SPI.transfer(digit3);
+  SPI.transfer(digit4);
+  digitalWrite(SS, HIGH); // high to deselect
+}
+
+void spi_out_number(unsigned short num, unsigned short base) {
+  unsigned short digit[4] = { 0, ' ', ' ', ' ' };
+  if ( (base<2) || (base>16) || (num>(base*base*base*base-1)) ) {
+    spi_out_display('x', 'x', 'n', 'o');  // indicates overflow
+  } else {
+    unsigned short place = 0;
+    while ( num && place < 4 ) {
+      if ( num > 0 ) digit[place++] = num%base;
+      num /= base;
+    }
+
+    spi_out_display(digit[3], digit[2], digit[1], digit[0]);
+  }
 }
 
 //The setup function is called once at startup of the sketch
@@ -22,17 +53,20 @@ void setup() {
   pinMode(sonar_pwm_pin, INPUT);
 
   SPI.begin(); //Start the SPI hardware
-  SPI.setClockDivider(SPI_CLOCK_DIV64); //Slow down the master a bit
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider(SPI_CLOCK_DIV64); //Slow down the master 1/64
 
   //Send the reset command to the display - this forces the cursor to return to the beginning of the display
-  digitalWrite(csPin, LOW); //Drive the CS pin low to select OpenSegment
-  SPI.transfer('v'); //Reset command
+  spi_out_command('v', ' '); //Reset command
+
+  spi_out_command(0x7A, 0x20); // brightness setup (will wear out the flash or eeprom with every write).
 }
 
 // The loop function is called in an endless loop
 void loop() {
   int cms = pwm_val();
   Serial.println(cms);
+  spi_out_number(cms, 10);
   delay(100);
 
 }
